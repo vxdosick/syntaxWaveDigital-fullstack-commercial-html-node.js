260a04d4f4d63735f67d7d9f676b9e965ccc39c3
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-08-20 23:25:03 +0300

    feat: Configure and adding cors

--- server/dev/app.ts
+++ server/dev/app.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import { router } from "./routes/emailRouter";
+const cors = require('cors');
 import express from "express";
 import path from "path";
 import BodyParser from "body-parser";
 
+
 const app: express.Application = express();
+app.use( cors() );
 const PORT = process.env.PORT || 3000;
 app.use( BodyParser.urlencoded({ extended: true }) );
 

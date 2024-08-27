b9a6f004eb9adcc1d739222f8a05170f4fe161ca
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-08-27 23:51:27 +0300

    feat: Add new service - Mail Marketing, add starting mjml config

--- server/src/app.js
+++ server/src/app.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const emailRouter_1 = require("./routes/emailRouter");
+const cors = require('cors');
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.static(path_1.default.join(__dirname, '../../client/')));
